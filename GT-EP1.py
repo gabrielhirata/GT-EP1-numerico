926d92c38c4519aca4798004a8c756faf05a27d3
Gabriel Hirata <gabrielhirata10@gmail.com>
2019-04-25 17:09:35 -0300

    primeira tarefa completa, falta conferir

--- GT-EP1.py
+++ GT-EP1.py
@@ -1,29 +1,37 @@
-# EP1 - Numérico  Gabriel Hirata e Thiago Takabatake
+# EP1 - Numerico  Gabriel Hirata e Thiago Takabatake
 import numpy as np     
 import math         
 
 # Primeira Tarefa
 
-def sincos(a,b):      # função que calcula os valores de sin(s) e cos(c)
-                      # a serem utilizados na Rotação de Givens
+def sincos(a,b):      # funcao que calcula os valores de sin(s) e cos(c)
+                      # a serem utilizados na Rotacao de Givens
     if abs(a)>abs(b):    # se |W[i][k]| > |W[j][k]|
         tau = -b/a       
         c = 1/math.sqrt(1+tau**2)
         s = c*tau
-    else:                # caso contrário
+    else:                # caso contrario
         tau = -a/b
         s = 1/math.sqrt(1+tau**2)
         c = s*tau
     return c,s
 
 def rotgivens(W,b,n,m,i,j,k,c,s):
+    # aplica a Rotacao de Givens nas linhas i e j da matriz W
     for r in range(k,m-1):
         aux = c*W[i][r] - s*W[j][r]
         W[j][r] = s*W[i][r] + c*W[j][r]
         W[i][r] = aux
+    # aplica a Rotacao de Givens nas linhas i e j do vetor b
     aux2 = c*b[i] - s*b[j]
     b[j] = s*b[i] + c*b[j]
     b[i] = aux2
+
+def somatorio(W,b,x,k):
+    soma = 0
+    for j in range(k+1,m-1):
+        soma = soma + W[k][j]*x[j]
+    return soma
 
 def fatqr(W,b,n,m):
     for k in range(0,m-1):
@@ -34,17 +42,17 @@ def fatqr(W,b,n,m):
                 q = W[j][k]
                 c,s = sincos(p,q)
                 rotgivens(W,b,n,m,i,j,k,c,s)
-#    for k in range(m,1,-1):
-#        x[k]=(b[k]-
+    for k in range(m-1,-1,-1):
+        x[k] = (b[k]- somatorio(W,b,x,k))/W[k][k]
 
 def criamatriz(n,m,item):    # função para criar as matrizes dos testes a) e b) 
                              # dados no enunciado
     if item == "a":
         W = np.zeros((n,m))  # cria matriz de zeros
         b = np.ones((n,1))     # cria matriz de "uns"
-        # laço para preencher a matriz conforme pedido no enunciado
-        # como índices de matrizes começam em 0 em Python, faz-se -1 nos índices de W
-        for i in range (1,n+1):  
+        # laco para preencher a matriz conforme pedido no enunciado
+        # como indices de matrizes começam em 0 em Python, faz-se -1 nos indices de W
+        for i in range(1,n+1):  
             for j in range(1,m+1):                
                 if abs(i-j)==1:
                     W[i-1][j-1]=1
@@ -54,16 +62,16 @@ def criamatriz(n,m,item):    # função para criar as matrizes dos testes a) e b
     if item == "b":
         W = np.zeros((n,m))      # cria matriz de zeros
         b = np.zeros((n,1))      # cria matriz de zeros
-        # laço para preencher a matriz W conforme pedido no enunciado
-        for i in range (1,n+1):
-            for j in range(1,m+1):
-                if abs((i-1)-(j-1))<=4:
-                    W[i-1][j-1]=1/(i+j-1)
-                else:
-                    W[i-1][j-1]=0
-        # laço para preencher a matriz b conforme pedido no enunciado
+        # laco para preencher a matriz W conforme pedido no enunciado
         for i in range(1,n+1):
-            b[i-1]=i
+            for j in range(1,m+1):
+                if abs((i-1)-(j-1)) <= 4:
+                    W[i-1][j-1] = 1/(i+j-1)
+                else:
+                    W[i-1][j-1] = 0
+        # laco para preencher a matriz b conforme pedido no enunciado
+        for i in range(1,n+1):
+            b[i-1] = i
     return W,b
 
 # realiza testes a) ou b)
@@ -72,18 +80,20 @@ if item == "a":
     n = 64
     m = 64
     W,b = criamatriz(n,m,item)
+    x = np.zeros((m,1))
 elif item == "b":
     n = 20
     m = 17
     W,b = criamatriz(n,m,item)
+    x = np.zeros((m,1))
 else:
-    print("Teste não existente.")
+    print("Teste nao existente.")
 
 print(W)
-print(b)
 fatqr(W,b,n,m)
 for i in range(0,n-1):
     for j in range(0, m-1):
         if i > j and W[i][j] != 0:
             print(W[i][j])
-print(b)
+#print(b)
+print(x)
