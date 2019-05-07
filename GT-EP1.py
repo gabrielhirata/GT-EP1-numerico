6f2a268cadbeac292956babbeca8be2d3f288b85
Gabriel Hirata <gabrielhirata10@gmail.com>
2019-05-06 23:00:01 -0300

    multisistemas funcinando

--- GT-EP1.py
+++ GT-EP1.py
@@ -1,8 +1,9 @@
-# EP1 - Numerico  Gabriel Hirata e Thiago Takabatake
+# Ep1 - Numerico  Gabriel Hirata e Thiago Takabatake
 import numpy as np     
 import math         
 
-# Primeira Tarefa
+#########################################################################################
+# primeira Tarefa
 
 def sincos(a,b):      # funcao que calcula os valores de sin(s) e cos(c)
                       # a serem utilizados na Rotacao de Givens
@@ -16,16 +17,12 @@ def sincos(a,b):      # funcao que calcula os valores de sin(s) e cos(c)
         c = s*tau
     return c,s
 
-def rotgivens(W,b,n,m,i,j,k,c,s):
-    # aplica a Rotacao de Givens nas linhas i e j da matriz W
+def rotgivens(X,n,m,i,j,k,c,s):
+    # aplica a Rotacao de Givens nas linhas i e j da matriz X
     for r in range(k,m):
-        aux = c*W[i][r] - s*W[j][r]
-        W[j][r] = s*W[i][r] + c*W[j][r]
-        W[i][r] = aux
-    # aplica a Rotacao de Givens nas linhas i e j do vetor b
-    aux2 = c*b[i] - s*b[j]
-    b[j] = s*b[i] + c*b[j]
-    b[i] = aux2
+        aux = c*X[i][r] - s*X[j][r]
+        X[j][r] = s*X[i][r] + c*X[j][r]
+        X[i][r] = aux
 
 def somatorio(W,x,k):
     soma = 0
@@ -38,10 +35,11 @@ def fatqr(W,b,n,m):
         for j in range(n-1,k,-1):
             i = j-1
             if W[j][k] != 0:
-                p = W[i][k]
-                q = W[j][k]
-                c,s = sincos(p,q)
-                rotgivens(W,b,n,m,i,j,k,c,s)
+                e = W[i][k]
+                f = W[j][k]
+                c,s = sincos(e,f)
+                rotgivens(W,n,m,i,j,k,c,s)
+                rotgivens(b,n,1,i,j,0,c,s)
     for k in range(m-1,-1,-1):
         x[k] = (b[k]- somatorio(W,x,k))/W[k][k]
 
@@ -51,7 +49,7 @@ def criamatriz(n,m,item):    # função para criar as matrizes dos testes a) e b
         W = np.zeros((n,m))    # cria matriz de zeros
         b = np.ones((n,1))     # cria matriz de "uns"
         # laco para preencher a matriz conforme pedido no enunciado
-        # como indices de matrizes começam em 0 em Python, faz-se -1 nos indices de W
+        # como indices de matrizes começam em 0 em python, faz-se -1 nos indices de W
         for i in range(1,n+1):  
             for j in range(1,m+1):                
                 if abs(i-j)==1:
@@ -89,12 +87,81 @@ elif item == "b":
 else:
     print("Teste nao existente.")
 
-print(W)
 fatqr(W,b,n,m)
-# for i in range(0,n-1):
-#     for j in range(0, m-1):
-#         if i > j and W[i][j] != 0:
-#             print(W[i][j])
-#print(b)
-
 print(x)
+
+def somatorio2(W,H,k,j):
+    soma = 0
+    for i in range(k+1,p):
+        soma = soma + W[k][i]*H[i][j]
+    return soma
+
+def variossistemas(n,p,m,W,A,H):
+    for k in range(p):
+        for j in range(n-1,k,-1):
+            i = j-1
+            if W[j][k] != 0:
+                e = W[i][k]
+                f = W[j][k]
+                c,s = sincos(e,f)
+                rotgivens(W,n,p,i,j,k,c,s)
+                rotgivens(A,n,m,i,j,0,c,s)
+    for k in range(p-1,-1,-1):
+        for j in range(m):
+            H[k][j] = (A[k][j]- somatorio2(W,H,k,j))/W[k][k]
+
+def criamatriz2(n,p,m,item):    # função para criar as matrizes dos testes a) e b) 
+                                # dados no enunciado
+    if item == "c":
+        W = np.zeros((n,p))    # cria matriz de zeros
+        A = np.ones((n,m))     # cria matriz de "uns"
+        # laco para preencher a matriz conforme pedido no enunciado
+        # como indices de matrizes começam em 0 em python, faz-se -1 nos indices de W
+        for i in range(1,n+1):  
+            for j in range(1,p+1):                
+                if abs(i-j)==1:
+                    W[i-1][j-1]=1
+                else:
+                    W[i-1][j-1]=0
+                W[i-1][i-1]=2
+        for i in range(1,n+1):        
+            A[i-1][1] = i
+            A[i-1][2] = 2*i-1
+    if item == "d":
+        W = np.zeros((n,p))      # cria matriz de zeros
+        A = np.ones((n,m))       # cria matriz de "uns"
+        # laco para preencher a matriz W conforme pedido no enunciado
+        for i in range(1,n+1):
+            for j in range(1,p+1):
+                if abs((i-1)-(j-1)) <= 4:
+                    W[i-1][j-1] = 1/(i+j-1)
+                else:
+                    W[i-1][j-1] = 0
+        # laco para preencher a matriz b conforme pedido no enunciado
+        for i in range(1,n+1):        
+            A[i-1][1] = i
+            A[i-1][2] = 2*i-1
+    return W,A
+
+# realiza testes c) ou d)
+item = input("Qual o item ('c' ou 'd') a ser testado: ")  
+if item == "c":
+    n = 64
+    p = 64
+    m = 3
+    W,A = criamatriz2(n,p,m,item)
+    H = np.zeros((p,m))
+elif item == "d":
+    n = 20
+    p = 17
+    m = 3
+    W,A = criamatriz2(n,p,m,item)
+    H = np.zeros((p,m))
+else:
+    print("Teste nao existente.")
+
+variossistemas(n,p,m,W,A,H)
+print(H)
+
+#########################################################################################
+# Segunda Tarefa
